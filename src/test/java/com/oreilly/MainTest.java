dcf730b0ac4c3630855e8b6ba29ea793372329f9
sreesuresh <71821210+sreesuresh@users.noreply.github.com>
2023-10-19 14:24:53 +0100

    New maven project created

---
+++ src/test/java/com/oreilly/MainTest.java
@@ -0,0 +1,12 @@
+package com.oreilly;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMyApp() {
+        assertEquals("Hello, Maven!", "Hello, Maven!");
+    }
+}
